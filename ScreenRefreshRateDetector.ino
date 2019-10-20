f504fdc1b5233a16914b2a7bd985c364f9146164
Johan Vandegriff <johan@vandymail.com>
2019-10-19 22:59:48 -0400

    running on the ArduinOLED board

--- ScreenRefreshRateDetector.ino
+++ ScreenRefreshRateDetector.ino
@@ -1,26 +1,53 @@
 /*
-  Screen Refresh Rate Detector
+ * Screen Refresh Rate Detector
+ *
+ * This sketch is for the ArduinOLED board. for more info, visit:
+ * 
+ * Johan.Vandegriff.net/ArduinOLED
+ * 
+ * It tests all the parts of the board:
+ *   Screen
+ *   Buttons
+ *   Joystick
+ *   Buzzer
+ */
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+#include <Arduino.h>
+#include <U8x8lib.h> //library to control the display in character mode
+#include <pitches.h> //list of pitches for the buzzer
+#include <ArduinOLED.h> //library to read the buttons and joysticks
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+//set up the display
+//https://github.com/olikraus/u8g2/wiki/u8x8setupcpp
+U8X8_SSD1306_128X64_NONAME_HW_I2C display(/* reset=*/ U8X8_PIN_NONE);
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
 
-  This example code is in the public domain.
+/*
+ * function to display test to the screen
+ * implements wrap-around and inverted text
+ * if it reaches the end of the last line, the rest of the text is not shown
+ */
+void output(uint8_t y, uint8_t x, String text, boolean isInverted) {
+  display.setInverseFont(isInverted);
+  uint8_t i = x;
+  uint8_t j = y;
+  uint8_t n = 0;
+  while (j < 8 && n < text.length()) {
+    display.setCursor(i, j);
+    display.print(text.charAt(n));
+    n++;
+    i++;
+    if (i >= 16) {
+      i = 0;
+      j++;
+    }
+  }
+}
 
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
+
+
+
+
 
 #define HISTORY_LENGTH 32
 uint16_t history[HISTORY_LENGTH];
@@ -32,15 +59,80 @@ uint16_t lastVal, val, minVal, maxVal, avgVal;
 int16_t diff, avgDiff;
 float dist, freq;
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  ArduinOLED.begin(); //start the ArduinOLED library, which controls the buttons and joystick
+  display.begin(); //start the u8x8, which controls the display in character mode
+  display.setFont(u8x8_font_amstrad_cpc_extended_f); //set the font
+//  display.setFont(u8x8_font_chroma48medium8_r); //an alternate font. there are a lot more of these available
+  display.clear(); //clear the display
+  //output a long string of 8*16 zeroes using inverted text.
+  //the F("string") function stores strings in flash memory. More at the bottom of this page:
+  //https://www.arduino.cc/en/Reference/PROGMEM
+  output(0, 0, F("                                                                                                                                "), true );
+//  delay(1000); //wait for 1 second
+  display.clear(); //clear the display
+//  //display the info text
+//  output(0, 0, F("ArduinOLED v1.0 "), true );
+//  output(2, 0, F("Johan.Vandegriff"), false);
+//  output(3, 0, F(".net/ArduinOLED "), false);
+//  output(5, 0, F("  Test Program  "), true );
+//  //wait for each button to be pressed, then play a tone
+//  output(7, 0, F("Press A         "), false);
+//  tone(BUZZER_PIN, NOTE_G3, 100); //BUZZER_PIN is defined as pin 3 in the ArduinOLED library
+//  ArduinOLED.pause(BTN_A);
+//  tone(BUZZER_PIN, NOTE_A3, 100);
+//  output(7, 6, F("B"), false);
+//  ArduinOLED.pause(BTN_B);
+//  tone(BUZZER_PIN, NOTE_B3, 100);
+//  output(7, 6, F("X"), false);
+//  ArduinOLED.pause(BTN_X);
+//  tone(BUZZER_PIN, NOTE_C4, 100);
+//  output(7, 6, F("Y"), false);
+//  ArduinOLED.pause(BTN_Y);
+//  tone(BUZZER_PIN, NOTE_D4, 100);
+//  output(7, 6, F("H"), false);
+//  ArduinOLED.pause(BTN_H);
+//  tone(BUZZER_PIN, NOTE_E4, 100);
+//  output(7, 6, F("L"), false);
+//  ArduinOLED.pause(BTN_L);
+//  tone(BUZZER_PIN, NOTE_FS4, 100);
+//  output(7, 6, F("R"), false);
+//  ArduinOLED.pause(BTN_R);
+//  tone(BUZZER_PIN, NOTE_G4, 100);
+//  output(7, 6, F("U"), false);
+//  ArduinOLED.pause(BTN_U);
+//  tone(BUZZER_PIN, NOTE_A4, 100);
+//  output(7, 6, F("D"), false);
+//  ArduinOLED.pause(BTN_D);
+//  tone(BUZZER_PIN, NOTE_B4, 100);
+//  output(6, 0, F("Press Z (joy-   stick button)"), false);
+//  ArduinOLED.pause(BTN_Z);
+//  tone(BUZZER_PIN, NOTE_C5, 100);
+//  output(6, 0, F("Move the joy-   stick left   "), false);
+//  ArduinOLED.pause(BTN_JOY_L);
+//  tone(BUZZER_PIN, NOTE_D5, 100);
+//  output(7, 6, F("right"), false);
+//  ArduinOLED.pause(BTN_JOY_R);
+//  tone(BUZZER_PIN, NOTE_E5, 100);
+//  output(7, 6, F("up   "), false);
+//  ArduinOLED.pause(BTN_JOY_U);
+//  tone(BUZZER_PIN, NOTE_FS5, 100);
+//  output(7, 6, F("down"), false);
+//  ArduinOLED.pause(BTN_JOY_D);
+//  tone(BUZZER_PIN, NOTE_G5, 100);
+//
+//  //after the buttons have all been pressed, show the joystick values
+//  output(6, 0, F("Joystick X:     "), false );
+//  output(7, 0, F("Joystick Y:     "), false );
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
+
+String str;
+
 void loop() {
+
   history[i] = analogRead(A0);
   lastTime = currTime;
   currTime = millis();
@@ -116,8 +208,39 @@ void loop() {
   //  Serial.print(5*onOff);
   //
   Serial.println();*/
+
+  str = freq;
+  str += " Hz      ";
+  if (i == 0 && freq != -1.0) output(1, 1, str, false);
+  Serial.print(history[i]);
+  Serial.print(",");
   Serial.println(freq);
   i++;
   if (i >= HISTORY_LENGTH) i = 0;
   delay(1);
+//
+//  
+//  //convert the joystick x and y to a string
+//  x = ArduinOLED.joyX;
+//  y = ArduinOLED.joyY;
+//
+//  //put spaces on the end of each string to make it 4 chars long
+//  if (x.length() == 1) {
+//    x += "   ";
+//  } else if (x.length() == 2) {
+//    x += "  ";
+//  } else if (x.length() == 3) {
+//    x += " ";
+//  }
+//  if (y.length() == 1) {
+//    y += "   ";
+//  } else if (y.length() == 2) {
+//    y += "  ";
+//  } else if (y.length() == 3) {
+//    y += " ";
+//  }
+//
+//  //display the strings
+//  output(6, 12, x, false);
+//  output(7, 12, y, false);
 }
